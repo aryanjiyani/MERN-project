7bd907c4e1ca18ff4936a5e2235f2dc29b168a61
aryanjiyani <aryanjiyani19@gmail.com>
2022-07-22 18:35:45 +0530

    comment section

---
+++ client/src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900 '>
+            About
+        </h1>
+
+        <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus ab, pariatur odio qui quisquam placeat quibusdam debitis? Nam id iusto aliquid dolor modi quia repellat nemo quibusdam! Corrupti delectus qui quisquam totam provident reiciendis rem id. Sapiente, incidunt impedit labore magni repudiandae qui? Iusto perferendis, sequi rerum placeat iste minima.</p>
+
+
+        <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus ab, pariatur odio qui quisquam placeat quibusdam debitis? Nam id iusto aliquid dolor modi quia repellat nemo quibusdam! Corrupti delectus qui quisquam totam provident reiciendis rem id. Sapiente, incidunt impedit labore magni repudiandae qui? Iusto perferendis, sequi rerum placeat iste minima.</p>
+
+        <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima cumque nostrum beatae voluptate quia debitis error, exercitationem sint reiciendis sunt consequuntur provident natus impedit voluptatibus, laudantium magnam temporibus sequi! Voluptate totam odit, laboriosam excepturi recusandae repellat magni laborum non rem facere! Architecto perferendis molestiae modi similique itaque odit. Perferendis architecto unde incidunt eius. Voluptatibus fugit nobis obcaecati velit molestias rem ipsam nemo ex officiis labore quo quae saepe assumenda corrupti quia, vitae ducimus libero facilis adipisci. Enim, est dolor? Quam enim neque dolor, delectus, iste temporibus, magnam necessitatibus doloremque quaerat nesciunt nostrum consectetur et tempora tempore eaque aliquid voluptatum.</p>
+    </div>
+  )
+}
+
+export default About
